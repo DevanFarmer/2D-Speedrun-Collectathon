7b6e6ce96e841a0385d63b457c64b9c1fcd048f5
DevanFarmer <devanf21@gmail.com>
2025-05-05 19:22:20 +0200

    Unsubscribes from events on disable again

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,7 +12,8 @@ public class GameManager : MonoBehaviour
 
     private void OnDisable()
     {
-        
+        EventBus.Unsubscribe<LevelCompletedEvent>(OnLevelCompleted);
+        EventBus.Unsubscribe<LevelFailedEvent>(OnLevelFailed);
     }
 
     private void Start()
