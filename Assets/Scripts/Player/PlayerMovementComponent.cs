804a73fa2053e98af14f2f5a67225e32e5968c37
DevanFarmer <devanf21@gmail.com>
2025-05-05 19:50:17 +0200

    Added boost and slow down functionality

--- Assets/Scripts/Player/PlayerMovementComponent.cs
+++ Assets/Scripts/Player/PlayerMovementComponent.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class PlayerMovementComponent : MonoBehaviour
 {
     Rigidbody2D rb;
+    
+    [SerializeField] float currentSpeed;
 
-    [SerializeField] float speed;
+    [Header("Speed Values")]
+    [SerializeField] bool canChangeSpeed;
+    [SerializeField] float normalSpeed;
+    [SerializeField] float boostSpeed;
+    [SerializeField] float slowDownSpeed;
 
+    [Header("Enable/Disable Movement")]
     [SerializeField] bool canMove;
 
     private void OnEnable()
@@ -24,6 +31,14 @@ public class PlayerMovementComponent : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0f;
+
+        currentSpeed = normalSpeed;
+    }
+
+    private void Update()
+    {
+        if (!canMove) return;
+        HandleInput();
     }
 
     void FixedUpdate()
@@ -34,12 +49,28 @@ public class PlayerMovementComponent : MonoBehaviour
 
     void MoveForward()
     {
-        rb.MovePosition(transform.position + transform.up * speed * Time.fixedDeltaTime);
+        rb.MovePosition(transform.position + transform.up * currentSpeed * Time.fixedDeltaTime);
     }
 
     void OnPauseChange(PauseChangeEvent e)
     {
         if (e.IsPaused) canMove = false;
         else canMove = true;
+    }
+
+    void HandleInput()
+    {
+        if (!canChangeSpeed) return;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentSpeed = boostSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            currentSpeed = slowDownSpeed;
+        }
+        else
+            currentSpeed = normalSpeed;
     }
 }
