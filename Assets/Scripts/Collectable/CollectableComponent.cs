7189fcd1b004c59082d1ae77a721a9465382ac16
DevanFarmer <devanf21@gmail.com>
2025-05-05 20:22:34 +0200

    Added collectable effect functionality on collect

--- Assets/Scripts/Collectable/CollectableComponent.cs
+++ Assets/Scripts/Collectable/CollectableComponent.cs
@@ -1,13 +1,21 @@
 using EventBusEventData;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CollectableComponent : MonoBehaviour
 {
+    [SerializeField] List<CollectableEffect> collectablEffects = new();
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag != "Player") return;
 
         EventBus.Publish(new CollectCollectableEvent());
+
+        foreach (CollectableEffect effect in collectablEffects)
+        {
+            effect.OnCollected();
+        }
 
         Destroy(gameObject);
     }
